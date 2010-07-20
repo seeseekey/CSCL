7195b54d8ec3cefaf383b82cad7cfcf6396abf7b
seeseekey <seeseekey@157b09d4-0768-11df-9bc7-f7babce8d823>
2010-07-20 10:12:01 +0000

    git-svn-id: https://cscl.googlecode.com/svn/trunk@47 157b09d4-0768-11df-9bc7-f7babce8d823

--- CSCL/FileSystem.cs
+++ CSCL/FileSystem.cs
@@ -25,7 +25,7 @@ namespace CSCL
         /// </summary>
         static char[] illegalChars=new char[] { '<', '>', ':', '"', '|', '\0', '\x1', '\x2', '\x3', '\x4', '\x5',
 			'\x6', '\x7', '\x8', '\x9', '\xA', '\xB', '\xC', '\xD', '\xE', '\xF', '\x10', '\x11', '\x12', '\x13',
-			'\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1A', '\x1B', '\x1C', '\x1D', '\x1E', '\x1F' };
+			'\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1A', '\x1B', '\x1C', '\x1D', '\x1E', '\x1F', '\xfffd' };
         #endregion
 
         #region Eigenschaften
