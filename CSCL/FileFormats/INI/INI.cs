cdc09f798c97becf7677b9dd30d7d9d52a4829de
seeseekey <seeseekey@157b09d4-0768-11df-9bc7-f7babce8d823>
2010-08-02 10:37:39 +0000

    git-svn-id: https://cscl.googlecode.com/svn/trunk@49 157b09d4-0768-11df-9bc7-f7babce8d823

---
+++ CSCL/FileFormats/INI/INI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CSCL.FileFormats.INI
+{
+	public class INISection
+	{
+		public string Name;
+		public Dictionary<string, string> Values;
+
+		public INISection()
+		{
+			Values=new Dictionary<string, string>();
+		}
+	}
+
+	public class INI
+	{
+		public List<INISection> Sections=new List<INISection>();
+
+		public INI()
+		{
+		}
+
+		public INI(string filename)
+		{
+			string[] lines=File.ReadAllLines(filename);
+
+			INISection currentSection=null;
+
+			for(int i=0; i<lines.Length; i++)
+			{
+				string tmp=lines[i].Trim();
+
+				if(tmp=="") continue;
+
+				if(tmp[0]=='[')
+				{
+					char[] trimchars= { '[', ']' };
+
+					INISection section=new INISection();
+					section.Name=tmp.Trim(trimchars);
+
+					Sections.Add(section);
+					currentSection=section;
+				}
+				else
+				{
+					if(currentSection!=null)
+					{
+						string[] splited = tmp.Split('=');
+
+						currentSection.Values.Add(splited[0], splited[1]);
+					}
+				}
+			}
+		}
+	}
+}
