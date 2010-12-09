7871e820892d1a6bbce4e562286e2646bcaaa6e0
seeseekey <seeseekey@157b09d4-0768-11df-9bc7-f7babce8d823>
2010-12-09 13:40:47 +0000

    git-svn-id: https://cscl.googlecode.com/svn/trunk@84 157b09d4-0768-11df-9bc7-f7babce8d823

---
+++ CSCL/Pair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCL
+{
+	public class Pair<T>
+	{
+		public T First;
+		public T Second;
+
+		public Pair()
+		{
+		}
+
+		public Pair(T first, T second)
+		{
+			First=first;
+			Second=second;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if(obj==null) return false;
+			Pair<T> o=(Pair<T>)obj;
+			return First.Equals(o.First)&&Second.Equals(o.Second);
+		}
+
+		public override int GetHashCode()
+		{
+			if((object)First==null)
+			{
+				if((object)Second==null) return 0;
+				return (-1)^Second.GetHashCode();
+			}
+
+			if((object)Second==null) return First.GetHashCode();
+			return First.GetHashCode()^Second.GetHashCode();
+		}
+	}
+}
