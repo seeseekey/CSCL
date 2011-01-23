3fd056ca955e23f124d01fb6d443ce82e69adb5e
seeseekey <seeseekey@157b09d4-0768-11df-9bc7-f7babce8d823>
2011-01-23 13:37:22 +0000

    git-svn-id: https://cscl.googlecode.com/svn/trunk@95 157b09d4-0768-11df-9bc7-f7babce8d823

---
+++ CSCL/Games/Manasource/Npc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace CSCL.Games.Manasource
+{
+	public class Npc //: IComparable
+	{
+		#region Statische Funktionen
+		public static List<Npc> GetNpcsFromXml(string filename)
+		{
+			List<Npc> ret=new List<Npc>();
+			XmlData npcfile=new XmlData(filename);
+			List<XmlNode> npcs=npcfile.GetElements("npcs.npc");
+
+			foreach(XmlNode i in npcs)
+			{
+				ret.Add(new Npc(i));
+			}
+
+			return ret;
+		}
+		#endregion
+
+		public int ID { get; private set; }
+		public string SpriteFilename { get; private set; }
+		public string ParticleFxFilename { get; private set; }
+
+		public Npc(XmlNode node)
+		{
+			foreach(XmlAttribute i in node.Attributes)
+			{
+				switch(i.Name.ToLower())
+				{
+					case "id":
+						{
+							ID=Convert.ToInt32(i.Value);
+							break;
+						}
+					default:
+						{
+							throw new NotImplementedException();
+						}
+				}
+			}
+
+			foreach(XmlNode i in node.ChildNodes)
+			{
+				switch(i.Name.ToLower())
+				{
+					case "sprite":
+						{
+							SpriteFilename=i.Value;
+							break;
+						}
+					case "particlefx":
+						{
+							ParticleFxFilename=i.Value;
+							break;
+						}
+					default:
+						{
+							throw new NotImplementedException();
+						}
+				}
+			}
+		}
+	}
+}
---
+++ CSCL/Games/Manasource/Particle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace CSCL.Games.Manasource
+{
+	public class Particle //: IComparable
+	{
+		#region Statische Funktionen
+		//public static Particle GetSpriteFromXml(string filename)
+		//{
+		//    List<Monster> ret=new List<Monster>();
+
+		//    XmlData monsterFile=new XmlData(filename);
+		//    List<XmlNode> sprite=monsterFile.GetElements("sprite");
+		//    return new Particle(sprite[0]);
+		//}
+		#endregion
+
+		//public List<Imageset> Imagesets { get; private set; }
+		//public List<Action> Actions { get; private set; }
+		//public List<Include> Includes { get; private set; }
+		//public string Name { get; private set; }
+		//public string Action { get; private set; }
+		//public int Variants { get; private set; }
+		//public int VariantOffset { get; private set; }
+
+		public Particle(XmlNode node)
+		{
+			//Actions=new List<Action>();
+			//Imagesets=new List<Imageset>();
+			//Includes=new List<Include>();
+
+			foreach(XmlAttribute i in node.Attributes)
+			{
+				switch(i.Name.ToLower())
+				{
+					//case "name":
+					//    {
+					//        Name=i.Value;
+					//        break;
+					//    }
+					//case "action":
+					//    {
+					//        Action=i.Value;
+					//        break;
+					//    }
+					//case "variants":
+					//    {
+					//        Variants=Convert.ToInt32(i.Value);
+					//        break;
+					//    }
+					//case "variant_offset":
+					//    {
+					//        VariantOffset=Convert.ToInt32(i.Value);
+					//        break;
+					//    }
+					default:
+						{
+							throw new NotImplementedException();
+						}
+				}
+			}
+
+			foreach(XmlNode i in node.ChildNodes)
+			{
+				switch(i.Name.ToLower())
+				{
+					//case "action":
+					//    {
+					//        Actions.Add(new Action(i));
+					//        break;
+					//    }
+					//case "imageset":
+					//    {
+					//        Imagesets.Add(new Imageset(i));
+					//        break;
+					//    }
+					//case "include":
+					//    {
+					//        Includes.Add(new Include(i));
+					//        break;
+					//    }
+					//case "#text": //wird ignoriert
+					//    {
+					//        i.Value
+					//        break;
+					//    }
+					default:
+						{
+							throw new NotImplementedException();
+						}
+				}
+			}
+		}
+	}
+}
