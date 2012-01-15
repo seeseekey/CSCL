138447445ee441076fb097e8040487437a3dc943
seeseekey <seeseekey@googlemail.com>
2012-01-15 16:09:04 +0100

    Add DoublePair (KeyValuePair).

---
+++ CSCL/DoublePair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSCL
+{
+	public class DoublePair<T, U>
+	{
+		public DoublePair()
+		{
+		}
+
+		public DoublePair(T first, U second)
+		{
+			First=first;
+			Second=second;
+		}
+
+		public T First { get; set; }
+		public U Second { get; set; }
+	}
+}
