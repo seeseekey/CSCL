59a5be3f7bd322981aa238e9e45696de69fdc431
seeseekey <seeseekey@googlemail.com>
2012-02-25 21:32:58 +0100

    Add file.

---
+++ CSCL/FileFormats/TMX/TilesetNotExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCL.FileFormats.TMX
+{
+	public class TilesetNotExistsException: Exception
+	{
+		public string Filename { get; set; }
+
+		public TilesetNotExistsException(string filename)
+		{
+			Filename=filename;
+		}
+	}
+}
