f1594e36e2e73dc6ca9e46a496b0fd8dde884158
seeseekey <seeseekey@gmail.com>
2014-07-02 20:13:55 +0200

    Add rest client.

---
+++ CSCL/Network/REST/HttpMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSCL.Network.REST
+{
+    public enum HttpMethod
+    {
+        GET,
+        POST,
+        PUT,
+        PATCH,
+        DELETE,
+        HEAD,
+        OPTIONS,
+        TRACE
+    }
+}
+
---
+++ CSCL/Network/REST/RestClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace CSCL.Network.REST
+{
+    public class RestClient
+    {
+        public string ContentType { get; set; }
+        public string EndPoint { get; set; }
+        public HttpMethod Method { get; set; }
+        public string PostData { get; set; }
+
+        public RestClient(string endpoint="", HttpMethod method=HttpMethod.GET, string postData="")
+        {
+            EndPoint = endpoint;
+            Method = method;
+            ContentType = "application/json";
+            PostData = postData;
+        }
+
+        public string Request(string parameters="")
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EndPoint + parameters);
+
+            request.Method = Method.ToString();
+            request.ContentLength = 0;
+            request.ContentType = ContentType;
+
+            if (!string.IsNullOrEmpty(PostData) && Method == HttpMethod.POST)
+            {
+                var bytes = Encoding.GetEncoding("iso-8859-1").GetBytes(PostData);
+                request.ContentLength = bytes.Length;
+
+                using (var writeStream = request.GetRequestStream())
+                {
+                    writeStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            using(HttpWebResponse response=(HttpWebResponse)request.GetResponse())
+            {
+                string ret="";
+
+                if(response.StatusCode!=HttpStatusCode.OK)
+                {
+                    string message=String.Format("Request failed. Received HTTP {0}", response.StatusCode);
+                    throw new Exception(message);
+                }
+
+                //Recieve the response
+                using(var responseStream=response.GetResponseStream())
+                {
+                    if(responseStream!=null)
+                    {
+                        using(var reader=new StreamReader(responseStream))
+                        {
+                            ret=reader.ReadToEnd();
+                        }
+                    }
+                }
+
+                return ret;
+            }
+        }
+    }
+}
